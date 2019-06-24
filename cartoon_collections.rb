a9b9e7a804bfb89b1508efd36e09a3cd5cf42167
Syed Imadi <sydeffect1994@gmail.com>
2019-06-24 08:40:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,8 @@ def roll_call_dwarves(dwarves)
 dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
 end
 
-def summon_captain_planet()
+def summon_captain_planet(planeteer_calls)
+  
   
 end
 
