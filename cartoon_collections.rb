dedd89093ab5b81444465912d7db11c53ef61fc3
Syed Imadi <sydeffect1994@gmail.com>
2019-06-24 08:40:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,12 +5,11 @@ dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
 end
 
 def summon_captain_planet(planeteer_calls)
-  
+  planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
   
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls#
 end
 
 def find_the_cheese# code an argument here
