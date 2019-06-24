ef7444642925c95d8b1aac5dbb150f67866bc621
Syed Imadi <sydeffect1994@gmail.com>
2019-06-24 08:37:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,5 @@
-def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves(dwarves)
+  # code an argument here
   # Your code here
   array.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
